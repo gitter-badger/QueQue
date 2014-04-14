d90e1f8c99f709ea3db95474f4b30fd3af4753b4
Lord Anton Hvornum <anton.hvo@gmail.com>
2014-04-14 09:40:19 +0200

    Added more comments and reworked the code a bit to be more efficient and clean. Also considering moving the 'access' control into it's own module.. will have to talk to tamm first tho so she's aware that the move is done so the front-end can adjust accordingly

--- backend/queue.php
+++ backend/queue.php
@@ -2,26 +2,32 @@
 	require_once('./functions.php');
 	$handle = new db();
 
-	// Temporary solution called $extra
-	// This entire block will be replaced by a switch case later on.
-	$extra = '{}';
+	$parameters = '{}';
+	// if 'next' is given, we can also query the backend for access permissions.
+	// the access is also verified in the backend but i'll leave the function here as an example.
 	if (isset($_GET['next']) && strpos($handle->query('{"access" : "' . $_SERVER['REMOTE_ADDR'] . '"}'), 'true') !== false) {
 		$do = 'next';
 	} else if (isset($_GET['max'])) {
+		// max returns the highest queue number (the last number in the ticket system)
 		$do = 'max';
-	} else if (isset($_GET['access'])) {
-		$do = 'access';
 	} else if (isset($_GET['history'])) {
+		// History returns the the last 10 items of the ticket system
+		// unless a offset is given, then it's position -10+offset that's retrieved.
 		$do = 'history';
 		if (isset($_GET['offset']))
-			$extra = '{"offset" : "' . $_GET['offset'] . '"}';
-	} else {
-		$do = 'current';
+			$parameters = '{"offset" : "' . $_GET['offset'] . '"}';
+	} else if (isset($_GET['access'])) {
+		// == Access checks weither or not the client IP has
+		// == access to modify/manipulate the ticket system.
+		// ==
+		// == Consider moving this into access.php or something similar.
+		// == It breaks the syntax of $do and the JSON data in general,
+		// == either rework the query system of queue.php or move this one
+		// == particular function into it's own atmosphere. 
+		$data = $handle->query('{"access" : "' . $_SERVER['REMOTE_ADDR'] . '"}');
 	}
 
-	if ($do == 'access')
-		$data = $handle->query('{"access" : "' . $_SERVER['REMOTE_ADDR'] . '"}');
-	else
-		$data = $handle->query('{"queue" : "' . $do . '", "parameters" : ' . $extra . '}');
+	if (isset($do))
+		$data = $handle->query('{"queue" : "' . $do . '", "parameters" : ' . $parameters . '}');
 	print $data;
 ?>
